b42df47a12ec12d0f766031c04a2699a044863e4
Zwogli <Mathiaskohler@web.de>
2023-10-18 20:38:04 +0200

    init router-outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', component: loginComponent, outlet: 'login' },
+  // { path: '/home', component: channelListComponent, outlet: 'aside-left'}, 
+  // { path: '/home', component: homeComponent, outlet: 'main'}, 
+  // { path: '/home', component: threadCompnent, outlet: 'aside-right'}, 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
