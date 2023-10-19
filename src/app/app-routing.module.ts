bb7131e9e53105f03a56d76d9be6a3e41f4594a1
Tristan Bierig <112637018+TristanBierig@users.noreply.github.com>
2023-10-19 20:20:05 +0200

    Baseline directory/files structure init
    Added necessary folders for baseline file structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  // { path: '', component: loginComponent, outlet: 'login' },
-  // { path: '/home', component: channelListComponent, outlet: 'aside-left'}, 
-  // { path: '/home', component: homeComponent, outlet: 'main'}, 
-  // { path: '/home', component: threadCompnent, outlet: 'aside-right'}, 
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
