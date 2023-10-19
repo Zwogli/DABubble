a8061c772fb373a67c627f37fd605ee509f98211
Tristan Bierig <112637018+TristanBierig@users.noreply.github.com>
2023-10-19 21:53:58 +0200

    Added material module
    Added a custom module for all material design specific API imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MaterialModule } from './custom_modules/material.module';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
@@ -9,11 +11,14 @@ import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MainChatComponent } from './components/components/main-chat/main-chat.component';
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MainChatComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MaterialModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
