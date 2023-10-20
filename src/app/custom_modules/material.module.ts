270a37de6a39b12770ff26790b0567e5004a318a
Zwogli <Mathiaskohler@web.de>
2023-10-20 20:38:46 +0200

    edit style navbar-left

--- src/app/custom_modules/material.module.ts
+++ src/app/custom_modules/material.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 
-import { MatIconModule } from '@angular/material/icon';
+import {MatIconModule} from '@angular/material/icon';
 import {MatExpansionModule} from '@angular/material/expansion';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
 
 const MaterialComponents = [
   MatIconModule,
   MatExpansionModule,
   MatFormFieldModule,
-  MatInputModule
+  MatInputModule,
+  MatButtonModule
 ];
 
 @NgModule({
