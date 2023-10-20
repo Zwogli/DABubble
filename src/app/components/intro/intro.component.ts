a70c0c0c7cf6f5b4b294fc565c16726c0c0576d9
Jörg Schmalgemeier <j.schmalgemeier@googlemail.com>
2023-10-20 21:33:09 +0200

    finished first version of intro animation

--- src/app/components/intro/intro.component.ts
+++ src/app/components/intro/intro.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.component.html',
   styleUrls: ['./intro.component.scss']
 })
-export class IntroComponent {
+export class IntroComponent implements OnInit {
+  animationToLeft = false;
+  animationToRight = false;
+  animationToTop = false;
+
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.animationToLeft = true;
+    }, 1000);
+
+    setTimeout(() => {
+      this.animationToRight = true;
+    }, 1500);
+
+    setTimeout(() => {
+      this.animationToTop = true;
+    }, 3000);
+  }
+
 
 }
