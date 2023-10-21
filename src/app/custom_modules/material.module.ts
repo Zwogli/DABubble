b43c691e73d9baa3f039de31a4677224bc0e5af7
Tristan Bierig <112637018+TristanBierig@users.noreply.github.com>
2023-10-21 16:34:49 +0200

    Layout of chat comp
    Added further layout and css to the chat view. Also added some global variables to use some of the necessary colors

--- src/app/custom_modules/material.module.ts
+++ src/app/custom_modules/material.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 
-const MaterialComponents = [MatIconModule, MatButtonModule];
+const MaterialComponents = [MatIconModule, MatButtonModule, MatDividerModule];
 
 @NgModule({
   imports: [MaterialComponents],
