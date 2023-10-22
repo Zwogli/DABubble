9d92fc55571c20ab41ada0d8df28332423330959
Tristan Bierig <112637018+TristanBierig@users.noreply.github.com>
2023-10-22 18:27:32 +0200

    Added first Firestore functions
    Baseline Layout to snaphot certain doc implelemted.
    
    BUT still buggy probably due to Firebase/AngularFire version problems

--- src/app/components/components/main-chat/main-chat.component.ts
+++ src/app/components/components/main-chat/main-chat.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FirestoreService } from 'src/app/services/firestore.service';
 
 @Component({
   selector: 'app-main-chat',
   templateUrl: './main-chat.component.html',
-  styleUrls: ['./main-chat.component.scss']
+  styleUrls: ['./main-chat.component.scss'],
 })
-export class MainChatComponent {
+export class MainChatComponent implements OnInit {
+  testChatId: string = 'XJs8a192fOAERg5D9kJg';
 
+  constructor(private fireService: FirestoreService) {}
+
+  ngOnInit() {}
+
+  ngOnDestroy() {
+    this.fireService.unsubSingleChat();
+  }
 }
--- src/app/services/firestore.service.ts
+++ src/app/services/firestore.service.ts
@@ -1,9 +1,46 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import {
+  Firestore,
+  onSnapshot,
+  doc,
+  collection,
+} from '@angular/fire/firestore';
+import { Unsubscribe } from '@angular/fire/auth';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FirestoreService {
+  firestore: Firestore = inject(Firestore);
 
-  constructor() { }
+  singleChat!: any;
+  private singleCustomerSubject = new BehaviorSubject<any>(this.singleChat);
+  singleChat$ = this.singleCustomerSubject.asObservable();
+
+  unsubSingleChat!: Unsubscribe;
+
+  constructor() {
+    this.startSubSingleChat('XJs8a192fOAERg5D9kJg');
+  }
+
+  ngOnDestroy() {
+    this.unsubSingleChat();
+  }
+
+  startSubSingleChat(docId: string) {
+    this.unsubSingleChat = this.subSingleChat(docId);
+  }
+
+  subSingleChat(docId: string) {
+    return onSnapshot(this.getSingleDocRef('chatRecords', docId), (chat) => {
+      this.singleChat = chat.data();
+      console.log('Chat data: ', chat.data());
+      console.log(this.singleChat);
+    });
+  }
+
+  getSingleDocRef(collId: string, docId: string) {
+    return doc(collection(this.firestore, collId, 'messages'), docId);
+  }
 }
