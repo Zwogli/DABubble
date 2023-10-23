01a81c2163282e1c432052075cbad2c2fb9c7264
Zwogli <Mathiaskohler@web.de>
2023-10-23 22:31:18 +0200

    init navbar.service / fix error typesrcipt version

--- src/app/components/component/navbar-header-mobile/navbar-header-mobile.component.ts
+++ src/app/components/component/navbar-header-mobile/navbar-header-mobile.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-navbar-header-mobile',
   templateUrl: './navbar-header-mobile.component.html',
-  styleUrls: ['./navbar-header-mobile.component.scss'],
+  styleUrls: ['./navbar-header-mobile.component.scss']
 })
+
 export class NavbarHeaderMobileComponent {
   showMenu: boolean = false;
 
-  constructor(public dialog: MatDialog) {}
+  constructor() {}
 
   toggleMenu() {
     this.showMenu = !this.showMenu;
