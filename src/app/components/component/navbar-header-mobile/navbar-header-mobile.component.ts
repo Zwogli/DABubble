e8dded683d70294b3469d8812b89403c76329184
Zwogli <Mathiaskohler@web.de>
2023-10-23 11:07:01 +0200

    test dialog menu-profil

--- src/app/components/component/navbar-header-mobile/navbar-header-mobile.component.ts
+++ src/app/components/component/navbar-header-mobile/navbar-header-mobile.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-navbar-header-mobile',
   templateUrl: './navbar-header-mobile.component.html',
-  styleUrls: ['./navbar-header-mobile.component.scss']
+  styleUrls: ['./navbar-header-mobile.component.scss'],
 })
 export class NavbarHeaderMobileComponent {
+  showMenu: boolean = false;
 
+  constructor(public dialog: MatDialog) {}
+
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
+  }
 }
