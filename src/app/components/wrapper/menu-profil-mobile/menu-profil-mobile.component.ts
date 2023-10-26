e87473b2ca9861c53c0703d86296d8efa1aada50
Mathias Kohler <123494012+Zwogli@users.noreply.github.com>
2023-10-26 22:56:09 +0200

    delete comments

--- src/app/components/wrapper/menu-profil-mobile/menu-profil-mobile.component.ts
+++ src/app/components/wrapper/menu-profil-mobile/menu-profil-mobile.component.ts
@@ -16,17 +16,6 @@ export class MenuProfilMobileComponent {
   toggleMenu(){
     this.navbarService.closeMenu();
   }
-  // toggleMenu(){
-  //   let menu: HTMLElement | null = document.getElementById('menu');
-  //   menu?.classList.remove('slide--up');
-  //   menu?.classList.add('slide--down');
-
-  //   setTimeout(() => {
-  //     this.navbarService.toggleMenu();
-  //   }, 1000);
-
-  //   menu?.classList.add('slide--up');
-  // }
 
   openDialogProfil(){
     this.dialog.open(DialogProfilComponent)
