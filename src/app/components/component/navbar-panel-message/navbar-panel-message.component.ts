686611b6f5c8bf828900eefcadc93fbdf903542a
Zwogli <Mathiaskohler@web.de>
2023-10-28 09:08:14 +0200

    start implement auth

--- src/app/components/component/navbar-panel-message/navbar-panel-message.component.ts
+++ src/app/components/component/navbar-panel-message/navbar-panel-message.component.ts
@@ -9,16 +9,19 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class NavbarPanelMessageComponent {
   panelOpenState: boolean = false;
-  private subscription: Subscription;
+  // private subscription: Subscription;
   currentUser: any;
 
   constructor(private authService: AuthService){
-    this.subscription = this.authService.currentUser$.subscribe(
-      user => {
-        this.currentUser = user;
-        console.log('show me user ',user);
+    // this.subscription = this.authService.currentUser$.subscribe(
+    //   user => {
+    //     this.currentUser = user;
+    //     console.log('show me user ',user);
         
-      })
+    //   })
+    this.currentUser = this.authService.currentUserId;
+    console.log('currentUserId ', this.currentUser);
+    
     }
 
   rotateArrow() {
