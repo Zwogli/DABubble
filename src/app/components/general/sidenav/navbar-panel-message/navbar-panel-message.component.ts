20a2c891c610f1840619d1e3ee22963ac5e89f05
Zwogli <Mathiaskohler@web.de>
2023-11-15 19:05:04 +0100

    error reading photoUrl

--- src/app/components/general/sidenav/navbar-panel-message/navbar-panel-message.component.ts
+++ src/app/components/general/sidenav/navbar-panel-message/navbar-panel-message.component.ts
@@ -1,6 +1,5 @@
 import { Component, inject } from '@angular/core';
 import { Firestore, QuerySnapshot, collection, doc, onSnapshot, query, where } from '@angular/fire/firestore';
-import { Subject, Subscription, of, takeUntil } from 'rxjs';
 import { Chat } from 'src/app/models/chat.class';
 import { User } from 'src/app/models/user.class';
 import { FirestoreService } from 'src/app/services/firestore.service';
@@ -12,27 +11,18 @@ import { NavbarService } from 'src/app/services/navbar.service';
   styleUrls: ['./navbar-panel-message.component.scss']
 })
 export class NavbarPanelMessageComponent {
+  panelOpenState: boolean = false; //material design
   firestore: Firestore = inject(Firestore);
-  panelOpenState: boolean = false;
-  // currentUserId: any;
-  // currentUser!: User;
-  // subCurrentUser!: User;
-  // private currentUserIsDestroyed$ = new Subject<boolean>();
-  // userInChatsArray: Chat[] = [];
-  // chatBetweenUserIds: string[] = [];
-  // chatUserData: User[] = [];
-  // chatsArray!:Chat[];
-  // cacheChatUserData!: User;
-
-  //!
-  loggedUser!:User;
-  loggedUserId = localStorage.getItem('userId');
-  user_images = '../../../assets/img/avatar_female-v1.png';
+  currentUser!:User;
+  currentUserId = localStorage.getItem('userId');
   chats!: Chat[];
-
   chatFilteredUserIds!:string[];
   chatsUserData!:User[];
-  //!
+  
+  users_photoUrl = '../../../assets/img/avatar_female-v1.png';
+  // users_photoUrl:string;
+  // users_onlinestatus:boolean;
+  // users_name:string;
 
   
   constructor(
@@ -41,19 +31,15 @@ export class NavbarPanelMessageComponent {
   ){
   }
   
-  ngOnInit(){
-    this.readLoggedUser(this.loggedUserId);
-    this.readChatsFromUser(this.loggedUserId);
-    // this.currentUser = this.firestoreService.currentUser;    
-    // this.setCurrentUser();
-    // this.setChatArray();
-    // this.setChatUserData();
+  async ngOnInit(){
+    this.readLoggedUser(this.currentUserId);
+    this.readChatsFromUser(this.currentUserId);
   }
   
   readLoggedUser(userId:string | null){
     if(userId != null){
       onSnapshot(doc(this.firestore, 'user', userId), (user: any) => {
-        this.loggedUser = user.data();
+        this.currentUser = user.data();
       });
     }else{
       console.log('Error find no userId');
@@ -82,9 +68,9 @@ export class NavbarPanelMessageComponent {
 
   getUserIdsFromChats() {
     this.chatFilteredUserIds = [];
-    this.chatFilteredUserIds.push(this.loggedUser.id);
+    this.chatFilteredUserIds.push(this.currentUser.id);
     this.chats.forEach((chat) => {
-     if(chat.id !== this.loggedUser.id){
+     if(chat.id !== this.currentUser.id){
        let filteredUserId = this.filterUserId(chat);
         this.chatFilteredUserIds.push(filteredUserId[0]);
      }
@@ -94,9 +80,8 @@ export class NavbarPanelMessageComponent {
 
   filterUserId(chat:Chat){
     return chat.chatBetween.filter(
-      (chatsUserId: string) => chatsUserId !== this.loggedUser.id);
+      (chatsUserId: string) => chatsUserId !== this.currentUser.id);
   }
-
 
   async getUserDataFromChats(){
     this.chatsUserData = [];
@@ -109,41 +94,11 @@ export class NavbarPanelMessageComponent {
     });
   }
 
-  retryLoadImage(user: User) {
+  retryLoadImage(user:User) {
     if (user) {
-      user.photoUrl = this.user_images;
+      user.photoUrl = this.users_photoUrl;
     }
   }
-
-  /*
-  ngOnDestroy() {
-    this.currentUserIsDestroyed$.next(true);
-  }
-
-  setChatArray(){
-    this.firestoreService.chatsArray$
-    .pipe(takeUntil(this.currentUserIsDestroyed$))
-    .subscribe((chatsArray: any) => {
-      this.chatsArray = chatsArray;
-    });
-  }
-
-  setChatUserData(){
-    this.firestoreService.chatUserData$
-    .pipe(takeUntil(this.currentUserIsDestroyed$))
-    .subscribe((chatUser: any) => {
-      this.chatUserData = chatUser;
-    } )
-  }
-  
-  setCurrentUser() {
-    this.firestoreService.currentUser$
-    .pipe(takeUntil(this.currentUserIsDestroyed$))
-    .subscribe((user: User) => {
-      this.currentUser = user;
-    } )
-  }
-  */
 
   toggleNewChat(){
     this.navbarService.menuSlideUp('menuNewChat');
