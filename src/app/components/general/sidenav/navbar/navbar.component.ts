29eebdc7b2407748e93b98f08daa9a83be773ed9
Zwogli <Mathiaskohler@web.de>
2023-12-02 10:01:26 +0100

    init breakpoint observer

--- src/app/components/general/sidenav/navbar/navbar.component.ts
+++ src/app/components/general/sidenav/navbar/navbar.component.ts
@@ -4,6 +4,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { Subject, Subscription, takeUntil } from 'rxjs';
 import { User } from 'src/app/models/user.class';
 import { AuthService } from 'src/app/services/auth.service';
+import { BreakpointObserverService } from 'src/app/services/breakpoint-observer.service';
 import { FirestoreService } from 'src/app/services/firestore.service';
 import { NavbarService } from 'src/app/services/navbar.service';
 
@@ -15,18 +16,24 @@ import { NavbarService } from 'src/app/services/navbar.service';
 export class NavbarComponent {
   showMenu: boolean = false;
   showMainChat: boolean = false;
+  mobileView: boolean = false;
   private subscription: Subscription;
   currentUserId:any;
   
   constructor(
     private authService: AuthService,
     private firestoreService: FirestoreService,
-    private navbarService: NavbarService, 
+    private navbarService: NavbarService,
+    public responsiveService: BreakpointObserverService, 
     public dialog: MatDialog,
     ){
       this.subscription = this.navbarService.showMenu$.subscribe(
         visible => {
           this.showMenu = visible;
+        });
+      this.subscription = this.responsiveService.mobileView$.subscribe(
+        visible => {
+          this.mobileView = visible;
         });
   }
 
