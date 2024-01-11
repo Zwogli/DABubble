26de6b7d27f32b586691c34fe37d623bc41cbcf2
Jörg Schmalgemeier <j.schmalgemeier@googlemail.com>
2024-01-11 14:31:54 +0100

    added imprint and data protection to all auth components

--- src/app/components/general/auth/choose-avatar/choose-avatar.component.ts
+++ src/app/components/general/auth/choose-avatar/choose-avatar.component.ts
@@ -10,6 +10,7 @@ import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { AuthService } from 'src/app/services/auth.service';
 import { FirestoreService } from 'src/app/services/firestore.service';
 import { ActivatedRoute } from '@angular/router';
+import { ResponsiveService } from 'src/app/services/responsive.service';
 
 @Component({
   selector: 'app-choose-avatar',
@@ -21,6 +22,7 @@ export class ChooseAvatarComponent implements OnInit, OnDestroy {
   avatarIsChoosen = false;
   public idFromUrl: any = '';
   userWillCloseWindow = false;
+  isDesktop!:boolean;
 
   @ViewChild('unchoosenAvatar') unchoosenAvatar!: ElementRef;
 
@@ -28,8 +30,17 @@ export class ChooseAvatarComponent implements OnInit, OnDestroy {
     public authService: AuthService,
     private fireStorage: AngularFireStorage,
     public firestoreService: FirestoreService,
-    private Route: ActivatedRoute
-  ) {}
+    private Route: ActivatedRoute,
+    private rs: ResponsiveService
+  ) {
+    this.rs.isDesktop$.subscribe((val) => {
+      if (val) {
+        this.isDesktop = true;
+      } else {
+        this.isDesktop = false;
+      }
+    });
+  }
 
   async ngOnInit() {
     await this.getIdFromUrl();
