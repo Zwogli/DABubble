1664702d7a2cee90d8327b3e4da3d47d664621da
Tristan Bierig <112637018+TristanBierig@users.noreply.github.com>
2024-01-23 22:53:28 +0100

    Revert "Standby live-update-sidebar"
    This reverts commit 2f967864070c807fa2459bc02d090e0185516916.

--- src/app/services/sidebar.service.ts
+++ src/app/services/sidebar.service.ts
@@ -7,7 +7,6 @@ import {
   getDoc,
   doc,
   getDocs,
-  onSnapshot,
 } from '@angular/fire/firestore';
 import { User } from '../models/user.class';
 import { FirestoreService } from './firestore.service';
@@ -18,7 +17,6 @@ import {
   userConverter,
 } from '../interfaces/firestore/converter';
 import { Chat } from '../models/chat.class';
-import { Unsubscribe } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root',
@@ -31,16 +29,12 @@ export class SidebarService implements OnDestroy {
   public currentUser!: User;
   public privateChats: Chat[] = [];
   public privateChatsPanelData: IMessagePanel[] = [];
-  public docIdsForQuerySnapUser: string[] = [];
-  public docIdsForQuerySnapRecord: string[] = [];
 
   public privateChatsPanelDataSubject: BehaviorSubject<IMessagePanel[]> =
     new BehaviorSubject<IMessagePanel[]>([]);
 
-  private unsubSidebarPanelUser!: Unsubscribe;
-
   constructor() {
-    this.firestoreService.currentUser$
+    this.firestoreService.currentUserSubject
       .pipe(takeUntil(this.currentUserIsDestroyed$))
       .subscribe((user: User) => {
         this.currentUser = user;
@@ -52,7 +46,6 @@ export class SidebarService implements OnDestroy {
     this.currentUserIsDestroyed$.next(true);
     this.currentUserIsDestroyed$.complete();
     this.privateChatsPanelDataSubject.complete();
-    this.unsubSidebarPanelUser();
   }
 
   async loadPrivateChats() {
@@ -80,12 +73,8 @@ export class SidebarService implements OnDestroy {
     this.loadUserFromPrivateChats();
   }
 
-  async loadUserFromPrivateChats() {
-    this.docIdsForQuerySnapUser = [];
-    this.docIdsForQuerySnapRecord = [];
-    console.log(this.privateChats);
-
-    this.privateChats.forEach((chat: Chat) => {
+  loadUserFromPrivateChats() {
+    this.privateChats.forEach(async (chat: Chat) => {
       if (
         // Determine if chat is chat of the user itself and if its already in the array
         chat.chatBetween.length === 1 &&
@@ -102,82 +91,38 @@ export class SidebarService implements OnDestroy {
       } else {
         // Chat is with another user
         const index = chat.chatBetween.indexOf(this.currentUser.id);
-        console.log(chat.chatBetween);
-
+        let userId;
         // Find index of the other user
-        if (chat.chatBetween.length > 1 && index === 0) {
-          this.docIdsForQuerySnapUser.push(chat.chatBetween[1]);
-          this.docIdsForQuerySnapRecord.push(chat.id);
+        if (index === 0) {
+          userId = chat.chatBetween[1];
         } else {
-          this.docIdsForQuerySnapUser.push(chat.chatBetween[0]);
-          this.docIdsForQuerySnapRecord.push(chat.id);
+          userId = chat.chatBetween[0];
         }
-      }
-      console.log(chat);
-    });
+        let userData: IMessagePanel;
 
-    try {
-      console.log('Array for query snap ', this.docIdsForQuerySnapUser);
-      console.log('Array for query snap ', this.docIdsForQuerySnapRecord);
-
-      const q = query(
-        collection(this.firestore, 'user'),
-        where('id', 'in', this.docIdsForQuerySnapUser)
-      ).withConverter(userConverter);
-
-      let userData: IMessagePanel;
-      this.unsubSidebarPanelUser = onSnapshot(q, (querySnapshot) => {
-        this.privateChatsPanelData = [];
-
-        let chatRecord: string;
-        querySnapshot.forEach((user) => {
-          user.data().activePrivateChats.forEach((str) => {
-            this.privateChats.includes(str);
-            chatRecord = str;
+        try {
+          await getDoc(
+            doc(this.firestore, 'user', userId).withConverter(userConverter)
+          ).then((user) => {
+            userData = {
+              id: chat.id,
+              chatWith: user.data()!,
+            };
+            if (
+              // Cancel if object is already in Template array
+              this.privateChatsPanelData.some(
+                (chat: IMessagePanel) => chat.id === userData.id
+              )
+            ) {
+              return;
+            } else {
+              this.privateChatsPanelData.push(userData);
+            }
           });
-
-          userData = {
-            id: chatRecord,
-            chatWith: user.data(),
-          };
-
-          console.log('User to push in render arr ', user.data());
-
-          if (userData.id === this.currentUser.id) {
-            this.privateChatsPanelData.unshift(userData);
-          } else {
-            this.privateChatsPanelData.push(userData);
-          }
-
-          console.log(
-            'before emit to render subject ',
-            this.privateChatsPanelData
-          );
-          // Emit to subject
-          this.privateChatsPanelDataSubject.next(this.privateChatsPanelData);
-        });
-      });
-    } catch (error) {}
-
-    // try {
-    //   await getDoc(
-    //     doc(this.firestore, 'user', userId).withConverter(userConverter)
-    //   ).then((user) => {
-    //     userData = {
-    //       id: chat.id,
-    //       chatWith: user.data()!,
-    //     };
-    //     if (
-    //       // Cancel if object is already in Template array
-    //       this.privateChatsPanelData.some(
-    //         (chat: IMessagePanel) => chat.id === userData.id
-    //       )
-    //     ) {
-    //       return;
-    //     } else {
-    //       this.privateChatsPanelData.push(userData);
-    //     }
-    //   });
-    // } catch (error) {}
+        } catch (error) {}
+      }
+    });
+    // Emit to subject
+    this.privateChatsPanelDataSubject.next(this.privateChatsPanelData);
   }
 }
